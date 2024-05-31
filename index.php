d6314731646110898f277fc00b7ef35d289c9871
Omar Juma <jumao2669@gmail.com>
2024-05-31 20:12:40 +0300

    doing advanced stuff

--- index.php
+++ index.php
@@ -3,36 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Let's Learn Php</title>
+    <title>Doing Advanced Stuff</title>
 </head>
 <body>
-    <h1>Let's explore Arrays in PHP</h1>
-    <pre>
-<?php
-    $programmingLanguages = ["PHP", "JavaScript", "Python"];
+    <h1>Let's Do Advanced Stuff </h1>
+    <strong>Getting the output using switch block</strong>
+    <p>switch statement is an alternative to multiple if else blocks, it performs loose comparisons such that 1 and '1' is equal and it requires a break statement to escape the current code execution block/ case </p> 
+    <?php 
+    $paymentStatus = 2;
+    switch ($paymentStatus){
+        case 1:
+            echo "Paid";
+            break;
+        case 2:
+        case 3:
+            echo "Payment Declined";
+            break;
+        default:
+        echo "Unknown Payment Status";
+    }
+    ?>
+    <br/>
+    <br/>
 
-    $programmingLanguages[3] = "C";
-    array_push($programmingLanguages, "Ruby","DotNet");
-    print_r($programmingLanguages);
+    <strong>Getting the output using match block</strong>
+    <p>match it uses key - value pairs, it is used when we are searching for a specific key, it can return a value/ expression or a function <strong style="color: red;">You can't use echo inside its block</strong></p>
+    <?php
 
-    $objectsArray = ['a'=>'A', 'b'=>'B', 'c'=>"C" ];
-    print_r($objectsArray);
-    echo $objectsArray["b"]."<br/>";
-
-    $authors = [
-        "name"=>"Eugene",
-        "age"=> 27,
-        "books" =>[
-            "The Color of Love",
-            "A trip to Paris",
-            "The richest man in Babylon"
-        ],
-        "isMarried"=>true
-    ];
-
-   print_r($authors["books"][1]);
-?>
-    </pre>
-    
+    match($paymentStatus){
+        1 => print ("Paid"),
+        2,3 => print ("Payment Declined"),
+        default => print("Unkown Payment Status")
+    }
+    ?>
 </body>
 </html>
